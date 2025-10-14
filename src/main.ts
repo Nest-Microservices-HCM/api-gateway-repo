48dc38ffa8a80bda8ed46d13b39ca53991f73531
Brayan Texis <brayan.eduardo1902@gmail.com>
2025-10-14 11:55:08 -0600

    Ajusta el orden de los logs en el archivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,10 @@ async function main() {
   );
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
-  
+
   await app.listen(envs.port);
 
   logger.log(`Gateway running on port:${envs.port}`);
+  console.log('Api gateway service');
 }
 main();
